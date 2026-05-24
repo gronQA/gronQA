cd4711130352434e54516ecc7c1643a3cda5fa0c
gronQA <gronqa@gmail.com>
2026-05-24 21:38:52 +0200

    update: reorder projects and update IDs

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -8,17 +8,17 @@ export interface Project {
 
 export const projects: Project[] = [
   { 
-    id: 3, 
-    title: 'Julia Polaczyk', 
-    category: 'Korepetycje z j. angielskiego', 
-    image: 'realizacje/polaczyk.png',
-    link: 'https://gronqa.github.io/juliapolaczyk/'
-  },
-  { 
-    id: 2, 
+    id: 1, 
     title: 'Kropla Bałtyku', 
     category: 'Apartament nad morzem', 
     image: 'realizacje/kropla.png',
     link: 'https://kropla-baltyku.pl'
+  },
+  { 
+    id: 2, 
+    title: 'Julia Polaczyk', 
+    category: 'Korepetycje z j. angielskiego', 
+    image: 'realizacje/polaczyk.png',
+    link: 'https://gronqa.github.io/juliapolaczyk/'
   },
 ];
