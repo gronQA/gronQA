418872b0146d5c5b03b70f6020af60973c35b7f0
gronQA <gronqa@gmail.com>
2026-06-04 20:48:01 +0200

    chore: update domain to gronka.pl

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gronqa.pl', // Adres strony
+  site: 'https://gronka.pl', // Adres strony
   integrations: [react()],
   build: {
     assets: 'assets' // CRITICAL: Avoids underscore folder issues on GitHub Pages
